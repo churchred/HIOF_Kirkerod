ee492e719f56ab092063072dacbf4886dd570583
churchred <98105070+churchred@users.noreply.github.com>
2025-01-17 12:32:04 +0100

    new

--- Oblig_1/src/Planet.java
+++ Oblig_1/src/Planet.java
@@ -1,7 +1,37 @@
+
+
+
+//     a) Lag en klasse som representerer en planet og kall denne Planet.
+//        Det vi ønsker å vite om en gitt planet er navn, radius og masse
+//        En planet skal kunne opprettes ved hjelp av en konstruktør på følgende måte:
+//
+//              Planet planet = new Planet("Mars", 3889.5, 6.39E23);
+//              (Husk å gjøre instansvariablene private, og lag get- og set-metoder.
+//              Da får vi en innkapsling av disse (ett av prinsippene i OOP).)
+
+
+
 public class Planet {
 
+    private String navn;
+    private double radius;
+    private double masse;
 
-    public static void main(String[] args) {
-
+    public Planet(String n, double rad, double ma){
+        navn = n;
+        radius = rad;
+        masse = ma;
     }
+
+    public void print(){
+        System.out.println("Planeten " + navn + " har en radius på " + radius + "km og en masse på " + masse + " kg");
+    }
+
+    private void setNavn(String navn){this.navn = navn;}
+    private void setRadius(double radius){this.radius = radius;}
+    private void setMasse(double masse){this.masse = masse;}
+
+    public String getNavn() {return navn;}
+    public double getRadius() {return radius;}
+    public double getMasse() {return masse;}
 }
--- Oblig_1/src/oppgave_2_3.java
+++ Oblig_1/src/oppgave_2_3.java
@@ -1,27 +1,42 @@
+
+
+
+// I denne oppgaven skal du lage to klasser.
+// En klasse som programmet kjøres fra og en klasse som definerer en
+// "custom datatype" du skal opprette objekter av.
+// Klassene er delt opp i de to følgende deloppgavene:
+//
+//     a) Lag en klasse som representerer en planet og kall denne Planet.
+//        Det vi ønsker å vite om en gitt planet er navn,
+//        radius og masse (den skal altså ha disse instansvariablene).
+//        En planet skal kunne opprettes ved hjelp av en konstruktør på følgende måte:
+//
+//              Planet planet = new Planet("Mars", 3889.5, 6.39E23);
+//              (Husk å gjøre instansvariablene private, og lag get- og set-metoder.
+//              Da får vi en innkapsling av disse (ett av prinsippene i OOP).)
+//
+//     b) En klasse kalt "Oppgave2_3.java".
+//        Denne må ha en main-metode slik at du får kjørt den.
+//        Du skal her instansiere noen Planet-objekter og deretter skrive ut
+//        informasjon om hver av disse til output på følgende format:
+//
+//             Planeten Mars har en radius på 3389.5 km og en masse på 6.39E23 kg
+
+
+
+
+
+
 public class oppgave_2_3 {
 
-    // I denne oppgaven skal du lage to klasser.
-    // En klasse som programmet kjøres fra og en klasse som definerer en
-    // "custom datatype" du skal opprette objekter av.
-    // Klassene er delt opp i de to følgende deloppgavene:
-    //
-    //     a) Lag en klasse som representerer en planet og kall denne Planet.
-    //        Det vi ønsker å vite om en gitt planet er navn,
-    //        radius og masse (den skal altså ha disse instansvariablene).
-    //        En planet skal kunne opprettes ved hjelp av en konstruktør på følgende måte:
-    //
-    //              Planet planet = new Planet("Mars", 3889.5, 6.39E23);
-    //              (Husk å gjøre instansvariablene private, og lag get- og set-metoder.
-    //              Da får vi en innkapsling av disse (ett av prinsippene i OOP).)
-    //
-    //     b) En klasse kalt "Oppgave2_3.java".
-    //        Denne må ha en main-metode slik at du får kjørt den.
-    //        Du skal her instansiere noen Planet-objekter og deretter skrive ut
-    //        informasjon om hver av disse til output på følgende format:
-    //
-    //             Planeten Mars har en radius på 3389.5 km og en masse på 6.39E23 kg
-
     public static void main(String[] args) {
+        Planet plan1 = new Planet("Mars", 3889.5, 6.39E23);
+        Planet plan2 = new Planet("Venus", 2822.5, 3.39E23);
+        Planet plan3 = new Planet("Neptun", 1889.5, 9.31E23);
+        plan1.print();
+        plan2.print();
+        plan3.print();
 
     }
+
 }
