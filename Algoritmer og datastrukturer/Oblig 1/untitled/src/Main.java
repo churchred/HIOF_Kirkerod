9b857f30471f6acee29545df55b7a26369d21d51
churchred <churchred@outlook.com>
2026-01-12 12:33:40 +0100

    chg

---
+++ Algoritmer og datastrukturer/Oblig 1/untitled/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+public class Main {
+
+    // List of all possible alphabet chars
+    private static char[] alphabet = {
+            'a','b','c','d','e','f','g','h','i','j',
+            'k','l','m','n','o','p','q','r','s','t',
+            'u','v','w','x','y','z'
+    };
+
+    // How many roations we move
+    private static int rotation = 13;
+
+
+    // Main loop where we test
+    public static void main(String[] args) {
+
+    }
+
+
+    // Encrypt the given message
+    public static String krypter(String S){
+
+        // Gets all the characters in the given String
+        char[] C = S.toCharArray();
+
+        // Loop through and
+        for (int i = 0; i < S.length(); i++)
+
+        return "False";
+    }
+
+    // Decrypt the given message
+    public static String dekrypter(String S){
+
+        return "False";
+    }
+}
