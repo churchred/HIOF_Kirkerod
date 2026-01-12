84ebb41a7827da8c3cee56cfec80c8624f7c23da
churchred <churchred@outlook.com>
2026-01-12 13:50:33 +0100

    Update Main.java

--- Algoritmer og datastrukturer/Oblig 1/untitled/src/Main.java
+++ Algoritmer og datastrukturer/Oblig 1/untitled/src/Main.java
@@ -1,39 +1,90 @@
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    // List of all possible alphabet chars
-    private static char[] alphabet = {
-            'a','b','c','d','e','f','g','h','i','j',
-            'k','l','m','n','o','p','q','r','s','t',
-            'u','v','w','x','y','z'
-    };
-
-    // How many roations we move
-    private static int rotation = 13;
-
-
-    // Main loop where we test
     public static void main(String[] args) {
 
+        // Gets user input
+        String line;
+        Scanner input = new Scanner(System.in);
+        System.out.print("$ ");
+        line = input.nextLine();
+
+        // Encrypts and prints the input string
+        String encryptedText = encrypt(line);
+        System.out.println("Encrypted: " + encryptedText);
     }
 
 
     // Encrypt the given message
-    public static String krypter(String S){
+    public static String encrypt(String S){
+
+        // Creates a stack and a queue
+        Stack<Character> stack = new Stack<Character>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Encrypts the given String, 0=move forwards(encrypt),
+        // while 1=move backwards(decrypt)
+        S = rot13(S);
+        System.out.println("> " + S);
 
         // Gets all the characters in the given String
         char[] C = S.toCharArray();
 
-        // Loop through and
-        for (int i = 0; i < S.length(); i++)
+        // Loop through all chars, encrypts them and places them
+        // in the new list in the right order.
+        for (int i = 0; i < C.length; i++){
 
-        return "False";
+            // First half of the chars are
+            // placed into a Queue and rest into a Stack.
+            if (i < C.length / 2){
+                queue.add(C[i]);
+            }
+            else{
+                stack.push(C[i]);
+            }
+        }
+
+
+        // Where we store all the new chars given from stack/queue
+        List<Character> list = new ArrayList<>();
+
+        // Alternates between adding from Queue and Stack until both empty
+        while (true){
+
+            // Take from stack if possible
+            if (!stack.isEmpty()) {
+                list.add(stack.pop());
+            }
+
+            // Take from queue if possible
+            if (!queue.isEmpty()) {
+                list.add(queue.poll());
+            }
+
+            // Check if both are empty and break loop if so
+            if (stack.isEmpty() && queue.isEmpty()) {
+                break;
+            }
+
+        }
+
+        return list.toString();
     }
 
-    // Decrypt the given message
-    public static String dekrypter(String S){
+    // Encrypt/Decrypt the given message
+    public static String rot13(String S){
 
-        return "False";
+        char[] C = S.toCharArray();
+        for (int i = 0; i < S.length(); i++)
+        {
+            char c = C[i];
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+            C[i] = c;
+        }
+        return String.valueOf(C);
     }
 }
