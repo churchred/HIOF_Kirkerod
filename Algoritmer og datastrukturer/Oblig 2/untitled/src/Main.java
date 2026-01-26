2d3e48aba717a25f49c1a93561446645bebd6a93
churchred <98105070+churchred@users.noreply.github.com>
2026-01-26 10:16:26 +0100

    oblig 2

--- Algoritmer og datastrukturer/Oblig 2/untitled/src/Main.java
+++ Algoritmer og datastrukturer/Oblig 2/untitled/src/Main.java
@@ -1,5 +1,8 @@
 import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Arrays;
 
 class Oblig_2
 {
@@ -30,10 +33,41 @@ class Oblig_2
     // skal utføre et antall multiplikasjoner som er lik n·log2n.
     public static void superlineær(long n){
         int tmp = 1;
+
+        // Lineær ytre løkke
         for (long i = 0; i < n; i++)
+            // Logaritmisk indre løkke
             for (long j = n; j > 0; j/=2)
                 tmp *= 1;
 
+    }
+
+    // skal utføre et antall multiplikasjoner som er lik n^3.
+    public static void kubisk(long n){
+        int tmp = 1;
+        for (long i = 0; i < n; i++){
+            for (long j = 0; j < n; j++){
+                for (long k = 0; k < n; k++){
+                    tmp *= 1;
+                }
+            }
+        }
+    }
+
+
+    // skal utføre et antall multiplikasjoner som er lik 2ⁿ
+    public static void eksponentiell(long n){
+        int tmp = 1;
+        for (long i = 0; i < n; i++)
+            tmp *= 1;
+    }
+
+    // skal utføre et antall multiplikasjoner som er lik n! (n-fakultet)
+    public static void kombinatorisk(long n){
+        int tmp = 1;
+        for (long i = 0; i < n; i++){
+            tmp *= 1;
+        }
     }
 
     public static void main(String[] args)
@@ -42,11 +76,44 @@ class Oblig_2
         long n, T, T1, T2;
         int valg, iterasjoner = 0;
 
-        System.out.print("1:O(n) 2:O(n²) 3:O(log_n) 4:O(n*log_n  ? ");
+        // Liste med navn på funksjonene (Brukes kun for terminal-pynting)
+        ArrayList<String> nameList = new ArrayList<>(
+                Arrays.asList(
+                        "O(n)",
+                        "O(n²)",
+                        "O(log n)",
+                        "O(n * log n)",
+                        "O(n³)",
+                        "O(2ⁿ)",
+                        "O(n!)"
+                )
+        );
+
+        // Valg av metode (antar bruker alltid angir gyldige verdier xD)
+        System.out.println("1:O(n) 2:O(n²) 3:O(log_n) 4:O(n*log_n) 5:O(n³) 6:O(2ⁿ) 7:O(n!)");
+        System.out.print("$ ");
         valg = S.nextInt();
-        System.out.print("n? ");
+        System.out.println("You chose: " + nameList.get(valg-1));
+        System.out.print("How big is n? ");
         n = S.nextLong();
 
+        // eksponentiell, så vil vi regne ut verdien for 2ⁿ FØR vi tar tiden på løkken
+        if (valg == 6){
+            double value = Math.pow(2, n);
+            n = (long) value;
+            System.out.println("   -->  (n³ = " + n + ")");
+        }
+        // kombinatorisk, så vil vi regne ut verdien for n! FØR vi tar tiden på løkken
+        if (valg == 7){
+            long nn = 1;
+            for (long i = 0; i < n; i++) {
+                nn *= (n - i);
+            }
+            n = nn;
+            System.out.println("   -->  (n! = " + n + ")");
+        }
+
+        // Tar tiden og bruker riktig metode
         T1 = System.currentTimeMillis();
         if (valg == 1)
             lineær(n);
@@ -56,10 +123,24 @@ class Oblig_2
             iterasjoner = logaritmisk(n);
         else if (valg == 4)
             superlineær(n);
+        else if (valg == 5)
+            kubisk(n);
+        else if (valg == 6)
+            eksponentiell(n);
+        else if (valg == 7)
+            kombinatorisk(n);
         T2 = System.currentTimeMillis();
 
+
+        // Hvis over 1 sekund så får vi tiden i sekunder med 2 gjeldene sifer
         T = T2 - T1;
-        System.out.print("T = " + T+ " ms");
+        if (T > 1000){
+            System.out.print("T = " + String.format("%.2f", T / 1000.0) + " s");
+        } else{
+            System.out.print("T = " + T+ " ms");
+        }
+
+        // Hvis vi har med iterasjoner i tellingen
         if (valg == 3)
             System.out.print(" (" + iterasjoner + ")");
         System.out.println();
