ced193f5509d25e264513cf6fa050a34aeb2be51
churchred <churchred@outlook.com>
2026-02-03 10:33:14 +0100

    oblig 3

---
+++ Algoritmer og datastrukturer/Oblig 3/Oppgave 1/src/Oppgave_1_3.java
@@ -0,0 +1,9 @@
+
+
+
+
+public class Oppgave_1_3 {
+    public static void main(String[] args) {
+        System.out.println("TEST 1");
+    }
+}
---
+++ Algoritmer og datastrukturer/Oblig 3/Oppgave 1/src/Oppgave_5_6.java
@@ -0,0 +1,7 @@
+
+
+public class Oppgave_5_6 {
+    public static void main(String[] args) {
+        System.out.println("TEST 2");
+    }
+}
